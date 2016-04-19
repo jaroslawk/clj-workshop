2b906b81592ba1f9fc29d69bdf679a5b024a3729
jaroslawk <jaroslaw.konczak@gmail.com>
2016-04-19 10:56:48 +0200

    func exers

--- src/clj_workshop/func_prog_exe.clj
+++ src/clj_workshop/func_prog_exe.clj
@@ -4,38 +4,51 @@
 (defn count-chars
   "Returns count of the provided character `c` in string `s`"
   [s c]
-  (unimplemented))
+  (reduce (fn [acc x]
+            (if (= c x)
+              (+ acc 1)
+              acc)) 0 s))
 
 (defn candidate-names
   [candidates]
-  (unimplemented))
+  (map :name candidates))
 
 (defn candidates-with-minimum-rating
   [min-rating candidates]
-  (unimplemented))
+  (filter (fn [{rating :rating}]
+             (> rating min-rating))
+           candidates))
 
 (defn palindrome?
   "Checks if the given sequence is a palindrome"
   [xs]
-  (unimplemented))
+  (loop [coll xs]
+    (let [f (first coll)
+          l  (last coll)]
+    (cond
+      (< (count coll) 2) true
+      (not (= l f)) false
+      true (recur (drop-last (drop 1 coll)))))))
 
 (defn count-palindromes
   [start end]
   (unimplemented))
 
+(defn is-prime? [n]
+  (let [range-to-n (range 2 n)]
+    (not-any? #(zero? (mod n %)) range-to-n)))
+
 (defn primes
   "Returns a lazy seq of prime numbers"
   []
-  (unimplemented))
+  (filter is-prime?
+          (drop 2 (range))))
 
-(defn flip-flop
-  [nums]
-  (unimplemented))
 
 (defn mungle-candidate-name
   "Returns uppercased and reversed name of the candidate"
-  [candidate]
-  (unimplemented))
+  [{name :name}]
+  (apply str (reverse (clojure.string/upper-case name))))
 
 (defn select-values
   "Selects keys from the provided map
@@ -53,12 +66,18 @@
 (defn power
   "Returns n to the power of m"
   [n m]
-  (unimplemented))
+  (loop [acc n counter m]
+    (if (= counter 1)
+      acc
+      (recur (* acc n) (dec counter)))))
 
 (defn range-sum
   "Calculates the sum of numbers [0..n)"
   [n]
-  (unimplemented))
+  (loop [acc 0 c 0]
+    (if (= c n)
+      acc
+      (recur (+ acc c) (inc c)))))
 
 (defn ratings-avg
   [candidates]
